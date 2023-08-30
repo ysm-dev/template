8209072ac2fb2f088661c83f2abda4e0d03b9593
ysm-dev <i@ysm.dev>
2023-08-31 00:37:16 +0900

    update template

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{ts,tsx}'],
+  content: ['./app/**/*.{ts,tsx}', './utils/**/*.{ts,tsx}'],
   theme: {
     extend: {},
   },
