626dc2bfe49ffd33a1ba80abf23c2c5a61e03316
ysm-dev <i@ysm.dev>
2024-06-13 01:00:55 +0900

    u

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({ children }: Props) {
           content="width=device-width, initial-scale=1, viewport-fit=cover"
         />
       </head>
-      <body>
+      <body className="antialiased">
         <Providers>{children}</Providers>
       </body>
     </html>
